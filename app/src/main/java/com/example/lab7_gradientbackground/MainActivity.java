794409c3720fc6ed210415d049aac02ff2235152
AndreyPC <tigra9900@mail.ru>
2021-09-16 20:20:43 +1000

    Комменты

--- app/src/main/java/com/example/lab7_gradientbackground/MainActivity.java
+++ app/src/main/java/com/example/lab7_gradientbackground/MainActivity.java
@@ -7,8 +7,9 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    // Метод, который вызывается, когда приложение создает и отображает Activity
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); //  вызов метода родительского класса
+        setContentView(R.layout.activity_main); // устанавливаем содержимое Activity из layout-файла
     }
 }
